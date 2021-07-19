cd91d8f144b174b40f76450e5bf6370aa0a3b903
RandyAlvarez0 <randyalvarez2020@gmail.com>
2021-07-18 20:26:24 -0400

    shopping cart

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    database: 'mongodb+srv://root:root@cluster0.latls.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    database: 'mongodb+srv://root:root@cluster0.latls.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    bookDatabase: 'mongodb+srv://root:root@cluster0.latls.mongodb.net/BooksDB.book?retryWrites=true&w=majority'
 }
