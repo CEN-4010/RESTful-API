e235cc21904966495d16fc8f3373281dee36cd41
Bryan Alvarenga <49573926+alvabryan@users.noreply.github.com>
2021-08-01 08:39:47 -0400

    Updated schema

--- models/wishList.js
+++ models/wishList.js
@@ -1,10 +1,10 @@
-const mongoose = require("moongoose")
-const book = require("../models/book")
+const mongoose = require("moongoose");
+const book = require("../models/book");
 
 const wishListSchema = mongoose.Schema({
         name: {type: String},
         userId: {type: String},
-        books: [book]
+        books: []
 });
 
-module.exports = mongoose.model('Wish List', wishListSchema);
+module.exports = mongoose.model('WishList', wishListSchema);
